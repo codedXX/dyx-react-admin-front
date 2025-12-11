e98ba4ec7200fcb89bdb671a9ad51bf71a85c2fc
codeYx <2584912193@qq.com>
2025-12-11 14:18:13 +0800

    修改api前缀

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://8.138.240.173:8080/api',
   timeout: 10000,
 });
 
