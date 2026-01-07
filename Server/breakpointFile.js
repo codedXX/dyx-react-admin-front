a174743264c615809f34a7b5792867e12f618715
codeYx <2584912193@qq.com>
2026-01-07 18:34:52 +0800

    修改

--- Server/breakpointFile.js
+++ Server/breakpointFile.js
@@ -13,3 +13,16 @@ const app = express();
 const port = 8101;
 //跨域
 app.use(cors());
+
+app.post("/api/upload/handshake", (req, res) => {
+  const fileInfo = req.body;
+  console.log("接受的信息", fileInfo);
+  res.json({
+    message: "你好",
+  });
+});
+
+// 启动服务器
+app.listen(port, () => {
+  console.log(`服务器运行在 http://localhost:${port}`);
+});
